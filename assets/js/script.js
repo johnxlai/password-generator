e0244b639caee47360d9da78a3e905f4f6cd5b81
John Lai <lai.john.py@gmail.com>
2023-01-18 22:44:11 -0500

    Add prompt function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,35 @@
 // Assignment code here
 // Get references to the #generate element
 const generateBtn = document.querySelector('#generate');
+const passwordText = document.querySelector('#password');
 
+function generatePassword() {
+  console.log('generate password');
+}
 // Write password to the #password input
 function writePassword() {
-  const password = generatePassword();
-  const passwordText = document.querySelector('#password');
-
-  passwordText.value = password;
+  askForPasswordCriterias();
+  // const password = generatePassword();
+  //Display password in box
+  // passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
 
 //Prompt question for password criteria
+function askForPasswordCriterias() {
+  //User selects which criteria to include in the password
+  alert('Please tell us your password criteria');
 
-//User selects which criteria to include in the password
+  const lenghtOfPw = prompt(
+    'Choose length of 8 characters and no more than 128 characters'
+  );
 
-//prompt for length of the password
-//Choose lenght of 8 characters and no more than 128 characters
+  console.log(lenghtOfPw);
+  //prompt for length of the password
+  //Choose length of 8 characters and no more than 128 characters
+}
 
 // Ask character types to include in the password
 //I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
@@ -26,5 +37,3 @@ generateBtn.addEventListener('click', writePassword);
 
 //All prompts are answered
 //Create a password
-
-// Display password in box

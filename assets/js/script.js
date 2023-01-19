3aff09142d11cf5ec303a9896370d038676b2297
John Lai <lai.john.py@gmail.com>
2023-01-18 22:31:05 -0500

    Add pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,30 @@
 // Assignment code here
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+const generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  const password = generatePassword();
+  const passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+//Prompt question for password criteria
+
+//User selects which criteria to include in the password
+
+//prompt for length of the password
+//Choose lenght of 8 characters and no more than 128 characters
+
+// Ask character types to include in the password
+//I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// (my input should be validated and at least one character type should be selected)
+
+//All prompts are answered
+//Create a password
+
+// Display password in box
